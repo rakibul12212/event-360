b3753ae39cc6f2bbb600d3b7f63ba6513c729ad2
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-06 18:23:46 +0600

    created pricing

---
+++ src/pages/home/Pricing/Pricing.tsx
@@ -0,0 +1,18 @@
+import Container from "@/components/ui/Container";
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <Container>
+      <p className="text-5xl font-bold">Explore our pricing plans</p>
+      <p className="text-sm">
+        Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh
+        ornare viverra. Ultrices faucibus neque velit risus ac id lorem.Ut
+        posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare
+        viverra. Ultrices faucibus neque velit risus ac id lorem.
+      </p>
+    </Container>
+  );
+};
+
+export default Pricing;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "./AboutSection";
 import HeroSection from "./HeroSection/HeroSection";
+import Pricing from "./Pricing/Pricing";
 
 const Home = () => {
   return (
     <div>
       <HeroSection></HeroSection>
       <AboutSection></AboutSection>
+      <Pricing></Pricing>
     </div>
   );
 };
