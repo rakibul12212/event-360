cfdf42d2c07f2affdbb973c0a55df527b4c8afba
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-07 02:59:27 +0600

    recent event created

---
+++ src/pages/home/RecentEvent/RecentEvent.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Container from "../../../components/ui/Container";
+import Button from "@/components/ui/Button/Button";
+const RecentEvent = () => {
+  return (
+    <Container>
+      <div className="flex justify-center items-center gap-[107px] pt-[131px]">
+        <div>
+          <h1 className="font-extrabold text-[48px] pb-[24px]">Recent Event</h1>
+          <p className="text-[18px] max-w-[518px] ">
+            Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh
+            ornare viverra. Ultrices faucibus neque velit risus ac id lorem.
+          </p>
+          <Button className="mt-[24px]">Learn more</Button>
+        </div>
+        <div className="grid grid-cols-3 gap-[20px] justify-items-center">
+          <img src="https://i.ibb.co/KbKHz1N/Rectangle-185.png" alt="" />
+          <img src="https://i.ibb.co/2sp4PJG/Rectangle-184.png" alt="" />
+          <img src="https://i.ibb.co/Mhz62vh/Rectangle-181.png" alt="" />
+          <img src="https://i.ibb.co/9GqZ8JN/Rectangle-182.png" alt="" />
+          <img src="https://i.ibb.co/NmRynyT/Rectangle-183.png" alt="" />
+          <img src="https://i.ibb.co/jLDZvWC/Rectangle-186.png" alt="" />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default RecentEvent;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,14 @@
 import AboutSection from "./AboutSection";
 import HeroSection from "./HeroSection/HeroSection";
 import Pricing from "./Pricing/Pricing";
+import RecentEvent from "./RecentEvent/RecentEvent";
 
 const Home = () => {
   return (
     <div>
       <HeroSection></HeroSection>
       <Pricing></Pricing>
+      <RecentEvent></RecentEvent>
       <AboutSection></AboutSection>
     </div>
   );
