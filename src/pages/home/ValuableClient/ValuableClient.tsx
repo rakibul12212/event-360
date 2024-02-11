402a62f9b347577e2c45b3cd77598cc35762079d
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-12 03:00:57 +0600

    updated

--- src/pages/home/ValuableClient/ValuableClient.tsx
+++ src/pages/home/ValuableClient/ValuableClient.tsx
@@ -8,19 +8,19 @@ const ValuableClient = () => {
         Our Valuable client
       </h1>
       {/* <Marquee> */}
-        <div className="grid grid-cols-5 gap-28 w-full pt-[103px]">
-          <img src="/public/assets/client/Company logo.png" alt="" />
-          <img src="/public/assets/client/Company logo (1).png" alt="" />
-          <img src="/public/assets/client/Company logo (2).png" alt="" />
-          <img src="/public/assets/client/Company logo (3).png" alt="" />
-          <img src="/public/assets/client/Company logo (4).png" alt="" />
+      <div className="grid grid-cols-5 gap-28 w-full pt-[103px]">
+        <img src="https://i.ibb.co/DkgHwST/Company-logo.png" alt="" />
+        <img src="https://i.ibb.co/5Y1BmqB/Company-logo-1.png" alt="" />
+        <img src="https://i.ibb.co/SmCbPs1/Company-logo-2.png" alt="" />
+        <img src="https://i.ibb.co/ww4J94z/Company-logo-3.png" alt="" />
+        <img src="https://i.ibb.co/NZCjMVV/Company-logo-4.png" alt="" />
 
-          <img src="/public/assets/client/Company logo (5).png" alt="" />
-          <img src="/public/assets/client/Company logo (6).png" alt="" />
-          <img src="/public/assets/client/Company logo (7).png" alt="" />
-          <img src="/public/assets/client/Company logo (8).png" alt="" />
-          <img src="/public/assets/client/Company logo (9).png" alt="" />
-        </div>
+        <img src="https://i.ibb.co/gJDs545/Company-logo-5.png" alt="" />
+        <img src="https://i.ibb.co/7vcJ2FQ/Company-logo-6.png" alt="" />
+        <img src="https://i.ibb.co/XsrJMQm/Company-logo-7.png" alt="" />
+        <img src="https://i.ibb.co/S3ySTQB/Company-logo-8.png" alt="" />
+        <img src="https://i.ibb.co/xLPB3Yp/Company-logo-9.png" alt="" />
+      </div>
       {/* </Marquee> */}
     </Container>
   );
