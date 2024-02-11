b80bdff8c3df237cce1fbddc569bad88b63439ae
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-12 03:57:36 +0600

    herosection animated

--- src/pages/home/HeroSection/HeroSection.tsx
+++ src/pages/home/HeroSection/HeroSection.tsx
@@ -1,24 +1,62 @@
 import Button from "@/components/ui/Button/Button";
 import Container from "@/components/ui/Container";
 import "./HeroSection.css";
+import { motion } from "framer-motion";
+
+const intro = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.25,
+      delayChildren: 1,
+    },
+  },
+};
+const introChildren = {
+  hidden: { opacity: 0, y: -200 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.7, type: "spring", bounce: 0.5 },
+  },
+};
 
 const HeroSection = () => {
   return (
     <div className="bg">
-      <Container className="text-center  text-white pt-28 md:pt-48 space-y-6 pb-64 ">
-        <p className="text-4xl md:text-5xl lg:text-6xl font-extrabold">
-          BRAND NEW EVENT PACKAGES
-        </p>
-        <p className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-[#FFD600]">
-          For Winter
-        </p>
-        <p className="text-md mx-auto max-w-[900px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga a vero
-          obcaecati aut totam delectus reprehenderit velit, consequuntur nostrum
-          aperiam quis non voluptatum voluptate nisi.
-        </p>
-
-        <Button>Explore</Button>
+      <Container>
+        <motion.div
+          className="text-center  text-white pt-28 md:pt-48 space-y-6 pb-64 "
+          variants={intro}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.p
+            className="text-4xl md:text-5xl lg:text-6xl font-extrabold"
+            variants={introChildren}
+          >
+            BRAND NEW EVENT PACKAGES
+          </motion.p>
+          <motion.p
+            className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-[#FFD600]"
+            variants={introChildren}
+          >
+            For Winter
+          </motion.p>
+          <motion.p
+            className="text-md mx-auto max-w-[900px]"
+            variants={introChildren}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga a vero
+            obcaecati aut totam delectus reprehenderit velit, consequuntur
+            nostrum aperiam quis non voluptatum voluptate nisi.
+          </motion.p>
+          <motion.div variants={introChildren}>
+            <Button>Explore</Button>
+          </motion.div>
+        </motion.div>
       </Container>
     </div>
   );
--- src/pages/home/ValuableClient/ValuableClient.tsx
+++ src/pages/home/ValuableClient/ValuableClient.tsx
@@ -1,27 +1,26 @@
-
 import Container from "@/components/ui/Container";
-// import Marquee from "react-fast-marquee";
+import Marquee from "react-fast-marquee";
 const ValuableClient = () => {
   return (
     <Container className="pt-[131px]">
       <h1 className="text-center font-extrabold text-[48px]">
         Our Valuable client
       </h1>
-      {/* <Marquee> */}
-      <div className="grid grid-cols-5 gap-28 w-full pt-[103px]">
-        <img src="https://i.ibb.co/DkgHwST/Company-logo.png" alt="" />
-        <img src="https://i.ibb.co/5Y1BmqB/Company-logo-1.png" alt="" />
-        <img src="https://i.ibb.co/SmCbPs1/Company-logo-2.png" alt="" />
-        <img src="https://i.ibb.co/ww4J94z/Company-logo-3.png" alt="" />
-        <img src="https://i.ibb.co/NZCjMVV/Company-logo-4.png" alt="" />
+      <Marquee>
+        <div className="grid grid-cols-5 gap-28 w-full pt-[103px]">
+          <img src="https://i.ibb.co/DkgHwST/Company-logo.png" alt="" />
+          <img src="https://i.ibb.co/5Y1BmqB/Company-logo-1.png" alt="" />
+          <img src="https://i.ibb.co/SmCbPs1/Company-logo-2.png" alt="" />
+          <img src="https://i.ibb.co/ww4J94z/Company-logo-3.png" alt="" />
+          <img src="https://i.ibb.co/NZCjMVV/Company-logo-4.png" alt="" />
 
-        <img src="https://i.ibb.co/gJDs545/Company-logo-5.png" alt="" />
-        <img src="https://i.ibb.co/7vcJ2FQ/Company-logo-6.png" alt="" />
-        <img src="https://i.ibb.co/XsrJMQm/Company-logo-7.png" alt="" />
-        <img src="https://i.ibb.co/S3ySTQB/Company-logo-8.png" alt="" />
-        <img src="https://i.ibb.co/xLPB3Yp/Company-logo-9.png" alt="" />
-      </div>
-      {/* </Marquee> */}
+          <img src="https://i.ibb.co/gJDs545/Company-logo-5.png" alt="" />
+          <img src="https://i.ibb.co/7vcJ2FQ/Company-logo-6.png" alt="" />
+          <img src="https://i.ibb.co/XsrJMQm/Company-logo-7.png" alt="" />
+          <img src="https://i.ibb.co/S3ySTQB/Company-logo-8.png" alt="" />
+          <img src="https://i.ibb.co/xLPB3Yp/Company-logo-9.png" alt="" />
+        </div>
+      </Marquee>
     </Container>
   );
 };
