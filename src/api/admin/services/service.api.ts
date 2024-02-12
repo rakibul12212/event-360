dedad0b7ad38a01e4222709c613b77aa30988241
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-13 03:56:58 +0600

    facing error

---
+++ src/api/admin/services/service.api.ts
@@ -0,0 +1,3 @@
+export const getServices = async () => {
+    return await fetch("http://localhost:5000/data").then((res) => res.json());
+  };
--- src/pages/Admin/ServiceList.tsx
+++ src/pages/Admin/ServiceList.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import { getServices } from "@/api/admin/services/service.api";
+import { useQuery } from "@tanstack/react-query";
 
 const ServiceList = () => {
-    return (
-        <div>
-            <p>service list</p>
+  const { data, isLoading } = useQuery({
+    queryKey: ["services"],
+    queryFn: getServices,
+  });
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+
+  console.log({ isLoading, data });
+
+  return (
+    <div>
+      {/* {data.map((item) => (
+        <div key={item.id}>
+          {item.data.map((service) => (
+            <div key={service.id}>
+              <h2>{service.title}</h2>
+            </div>
+          ))}
         </div>
-    );
+      ))} */}
+      {data.map((item) => (
+        <h1 key={item.id}>{item.title}</h1>
+      ))}
+    </div>
+  );
 };
 
 export default ServiceList;
