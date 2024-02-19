7da140084fb5eb59217aacf373c8d2823ced3b7a
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-19 20:09:18 +0600

    plans updated

--- src/pages/home/Pricing/Pricing.tsx
+++ src/pages/home/Pricing/Pricing.tsx
@@ -3,9 +3,17 @@ import CheckMark from "@/components/ui/CheckMark/CheckMark";
 import Container from "@/components/ui/Container";
 import useScrollGrowHook from "@/Hooks/scrollGrowHook";
 import { motion } from "framer-motion";
-
+import { getServices } from "@/api/admin/services/service.api";
+import { useQuery } from "@tanstack/react-query";
 const Pricing = () => {
   const { style, componentRef } = useScrollGrowHook();
+  const { data, isLoading } = useQuery({
+    queryKey: ["services"],
+    queryFn: getServices,
+  });
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
   return (
     <Container>
       <motion.div style={style} ref={componentRef}>
@@ -20,141 +28,42 @@ const Pricing = () => {
         </div>
         {/* card */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-[32px]">
-          <div>
-            <div className="border rounded-md hover:bg-[#EA9B00] shadow-xl">
-              <div className="px-5 py-5">
-                <h1 className="font-semibold text-lg pb-[20px]">Enterprice</h1>
-                <p className="text-sm max-w-[250px] pb-[20px]">
-                  In ornare ligula lorem, sit amet faucibus velit vehicula eget
-                </p>
-                <p className="font-bold text-[48px] pb-[20px]">
-                  <span className="text-[40px]">$</span>15
-                </p>
-                <Button className="p-2 mb-5 w-full bg-">
-                  Get this packages
-                </Button>
-                <ol className="font-semibold text-[12px] pb-[40px] space-y-2 ">
-                  <div className="flex gap-2 ">
-                    <CheckMark children={undefined} />
-                    <li>ten links available</li>
+          {data ? (
+            data[0].plans.map((plan) => (
+              <div key={plan["plans-id"]}>
+                <div className="border rounded-md hover:bg-[#EA9B00] shadow-xl">
+                  <div className="px-5 py-5">
+                    <h1 className="font-semibold text-lg pb-[20px]">
+                      {plan.header}
+                    </h1>
+                    <p className="text-sm max-w-[250px] pb-[20px]">
+                      {plan.title}
+                    </p>
+                    <p className="font-bold text-[48px] pb-[20px]">
+                      <span className="text-[40px]">$</span>
+                      {plan.price}
+                    </p>
+                    <Button className="p-2 mb-5 w-full bg-">
+                      Get this package
+                    </Button>
+                    <ol className="font-semibold text-[12px] pb-[40px] space-y-2">
+                      {plan.details.map((detail) => (
+                        <div className="flex gap-2" key={detail.id}>
+                          <CheckMark children={undefined} />
+                          <li>{detail.text}</li>
+                        </div>
+                      ))}
+                    </ol>
                   </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>Own analytics olatform</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>Mobile app</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>Chat support</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>1000 users</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>tamplate library</li>
-                  </div>
-                </ol>
+                </div>
+                <button className="w-full border py-3 bg-[#F2F2F2] hover:bg-gradient-to-r from-amber-400 to-amber-600 p-4 text-black cursor-pointer duration-300 ease-in-out px-10 py-2">
+                  See all features
+                </button>
               </div>
-            </div>
-            <button className="w-full border py-3 bg-[#F2F2F2] hover:bg-gradient-to-r from-amber-400 to-amber-600 p-4 text-black cursor-pointer  duration-300 ease-in-out px-10 py-2">
-              See all features
-            </button>
-          </div>
-          {/* card-2 */}
-          <div>
-            <div className="border rounded-md hover:bg-[#EA9B00] shadow-xl">
-              <div className="px-5 py-5">
-                <h1 className="font-semibold text-lg pb-[20px]">Forever</h1>
-                <p className="text-sm max-w-[250px] pb-[20px]">
-                  In ornare ligula lorem, sit amet faucibus velit vehicula eget
-                </p>
-                <p className="font-bold text-[48px] pb-[20px]">
-                  <span className="text-[40px]">$</span>25
-                </p>
-                <Button className="p-2 mb-5 w-full">Get this packages</Button>
-                <ol className="font-semibold text-[12px] pb-[40px] space-y-2">
-                  <div className="flex gap-2 ">
-                    <CheckMark children={undefined} />
-                    <li>ten links available</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>Own analytics olatform</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>Mobile app</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>Chat support</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>1000 users</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>tamplate library</li>
-                  </div>
-                </ol>
-              </div>
-            </div>
-            <button className="w-full border py-3 bg-[#F2F2F2] hover:bg-gradient-to-r from-amber-400 to-amber-600 p-4 text-black cursor-pointer  duration-300 ease-in-out px-10 py-2">
-              See all features
-            </button>
-          </div>
-          {/* card-3 */}
-          <div>
-            <div className="border rounded-md hover:bg-[#EA9B00] shadow-xl">
-              <div className="px-5 py-5">
-                <h1 className="font-semibold text-lg pb-[20px]">
-                  Business Pack
-                </h1>
-                <p className="text-sm max-w-[250px] pb-[20px]">
-                  In ornare ligula lorem, sit amet faucibus velit vehicula eget
-                </p>
-                <p className="font-bold text-[48px] pb-[20px]">
-                  <span className="text-[40px]">$</span>9
-                </p>
-                <Button className="p-2 mb-5 w-full">Get this packages</Button>
-                <ol className="font-semibold text-[12px] pb-[40px] space-y-2">
-                  <div className="flex gap-2 ">
-                    <CheckMark children={undefined} />
-                    <li>ten links available</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>Own analytics olatform</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>Mobile app</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>Chat support</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>1000 users</li>
-                  </div>
-                  <div className="flex gap-2">
-                    <CheckMark children={undefined} />
-                    <li>tamplate library</li>
-                  </div>
-                </ol>
-              </div>
-            </div>
-            <button className="w-full border py-3 bg-[#F2F2F2] hover:bg-gradient-to-r from-amber-400 to-amber-600 p-4 text-black cursor-pointer  duration-300 ease-in-out px-10 py-2">
-              See all features
-            </button>
-          </div>
+            ))
+          ) : (
+            <p className="text-center items-center">Loading...</p>
+          )}
         </div>
       </motion.div>
     </Container>
