5bdd004a16951186bc2512d18787a15246e710e3
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-19 19:08:06 +0600

    fixing tanstack data management

--- src/api/admin/services/service.api.ts
+++ src/api/admin/services/service.api.ts
@@ -1,3 +1,9 @@
+
+
 export const getServices = async () => {
     return await fetch("http://localhost:5000/data").then((res) => res.json());
   };
+
+
+
+
--- src/pages/Admin/ServiceList.tsx
+++ src/pages/Admin/ServiceList.tsx
@@ -1,37 +1,49 @@
-import { getServices } from "@/api/admin/services/service.api";
-import { useQuery } from "@tanstack/react-query";
+// import { getServices } from "@/api/admin/services/service.api";
+// import { useQuery } from "@tanstack/react-query";
+
+// const ServiceList = () => {
+//   const { data, isLoading } = useQuery({
+//     queryKey: ["services"],
+//     queryFn: getServices,
+//   });
+
+//   if (isLoading) {
+//     return <p>loading...</p>;
+//   }
+
+//   console.log({ isLoading, data });
+
+//   return (
+//     <div>
+//       <h1>All Data</h1>
+//       {data ? (
+//         <div>
+//           <h2>Services</h2>
+//           <ul>
+//             {data[0].services.map((service) => (
+//               <li key={service["services-id"]}>
+//                 <h3>{service.title}</h3>
+//                 <p>{service.text}</p>
+//                 <img src={service.image} alt="" />
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       ) : (
+//         <p>Loading...</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default ServiceList;
+
+import React from "react";
 
 const ServiceList = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["services"],
-    queryFn: getServices,
-  });
-
-  if (isLoading) {
-    return <p>loading...</p>;
-  }
-
-  console.log({ isLoading, data });
-
   return (
     <div>
-      <h1>All Data</h1>
-      {data ? (
-        <div>
-          <h2>Services</h2>
-          <ul>
-            {data[0].services.map((service) => (
-              <li key={service["services-id"]}>
-                <h3>{service.title}</h3>
-                <p>{service.text}</p>
-                <img src={service.image} alt="" />
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <p>service list</p>
     </div>
   );
 };
