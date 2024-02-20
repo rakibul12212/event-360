658fdf491d4fbf696b9eb3affaad5944c0285216
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-20 20:30:33 +0600

    netlify issue

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -19,7 +19,6 @@ const router = createBrowserRouter([
       { index: true, element: <Home></Home> },
       { path: "about", element: <About></About> },
       { path: "contact", element: <Contact></Contact> },
-      { path: "admin", element: <AdminLayout></AdminLayout> },
     ],
   },
   {
