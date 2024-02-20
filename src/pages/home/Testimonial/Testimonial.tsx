784cbfe38b3a233cd50ee2672d29dbafe2ca86d5
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-21 01:15:10 +0600

    d

--- src/pages/home/Testimonial/Testimonial.tsx
+++ src/pages/home/Testimonial/Testimonial.tsx
@@ -28,6 +28,29 @@ const Testimonial = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   const sliderRef = useRef<Slider>(null);
   const slideNext = () => {
